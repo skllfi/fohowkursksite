3aacada8ba9cbb95d112effe607401e3439bc153
skllfi <131898180+skllfi@users.noreply.github.com>
2025-10-24 15:17:06 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,7 @@
+// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "твоя_apiKey",
@@ -9,3 +11,4 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
